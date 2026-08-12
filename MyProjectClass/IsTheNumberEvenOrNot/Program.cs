d460e7a31484c5eb1cd528a549c49fe8c1568abd
sara berrio <saraberrio752@gmail.com>
2026-08-12 00:38:06 -0500

    complete exercise 02

---
+++ MyProjectClass/IsTheNumberEvenOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+string? numberstring;
+do
+{
+    Console.Write("Enter an integer or the word 'EXIT': ");
+     numberstring = Console.ReadLine();
+    if (numberstring!.ToUpper() == "EXIT")
+    {
+        continue;
+    }
+    var numberint = 0;
+    if (int.TryParse(numberstring, out numberint))
+
+        if (numberint % 2 == 0)
+        {
+            Console.WriteLine($" The number {numberint} is even ");
+        }
+        else
+        {
+            Console.WriteLine($" The number {numberint} is odd ");
+        }
+    else
+    {
+        Console.WriteLine($" The number {numberstring} is not a whole number ");
+    }
+
+} while (numberstring!.ToUpper() != "EXIT");
+Console.WriteLine("! Has ended ¡");
+
+
