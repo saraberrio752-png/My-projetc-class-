a7f33c0c47df8d1e6ae36d5f8c8ca35f05290539
sara berrio <saraberrio752@gmail.com>
2026-08-13 20:36:02 -0500

    completed exercise 02 

--- MyProjectClass/LargerNumber/Program.cs
+++ MyProjectClass/LargerNumber/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.Design;
-string? numberstringA;
-string? numberstringB;
-string? numberstringC;
+string? numberstringA=String.Empty;
+string? numberstringB=String.Empty;
+string? numberstringC= String.Empty;
 do
 {
     int numbera = 0;
@@ -10,49 +10,50 @@ do
 
     Console.WriteLine("Enter 3 different integers or the word 'EXIT': ");
     numberstringA = Console.ReadLine();
-    numberstringB = Console.ReadLine();
-    numberstringC = Console.ReadLine();
-
     if (numberstringA!.ToUpper() == "EXIT")
     {
         continue;
     }
+    numberstringB = Console.ReadLine();
     if (numberstringB!.ToUpper() == "EXIT")
     {
         continue;
     }
+    numberstringC = Console.ReadLine();
     if (numberstringC!.ToUpper() == "EXIT")
     {
         continue;
     }
-if (int.TryParse(numberstringA, out int a))
-{
-    numbera= a;
-}
-
-if (int.TryParse(numberstringB, out int b))
-{
-    numberb= b;
-}
-
-if (int.TryParse(numberstringC, out  int c))
-{
-     numberc= c;
-}
-
-if (numbera > numberb && numbera > numberc)
-{
-    Console.WriteLine($" The number {numbera} is the largest number ");
-}
-else
-    if (numberb > numbera && numberb > numberc)
+    if (int.TryParse(numberstringA, out int a))
     {
-        Console.WriteLine($" The number {numberb} is the largest number ");
+        numbera = a;
+    }
+
+    if (int.TryParse(numberstringB, out int b))
+    {
+        numberb = b;
+    }
+
+    if (int.TryParse(numberstringC, out int c))
+    {
+        numberc = c;
+    }
+
+    if (numbera > numberb && numbera > numberc)
+    {
+        Console.WriteLine($" The number {numbera} is the largest number ");
     }
     else
-    {
-        Console.WriteLine($" The number {numberc} is the largest number ");
-    }
+        if (numberb > numbera && numberb > numberc)
+        {
+            Console.WriteLine($" The number {numberb} is the largest number ");
+        }
+        else
+        {
+            Console.WriteLine($" The number {numberc} is the largest number ");
+        }
 
-} while (numberstringA!.ToUpper() != "EXIT");
+} while (numberstringA!.ToUpper() != "EXIT"
+         && numberstringB!.ToUpper() != "EXIT"
+          && numberstringC!.ToUpper() != "EXIT");
 Console.WriteLine("! Has ended ¡");
