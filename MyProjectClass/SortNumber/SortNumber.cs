b892d562357bcc589037ae97ed1e7b97009d77f0
sara berrio <saraberrio752@gmail.com>
2026-08-16 16:59:37 -0500

    Completed exercise 03

--- MyProjectClass/SortNumber/SortNumber.cs
+++ MyProjectClass/SortNumber/SortNumber.cs
@@ -8,25 +8,35 @@ do
     var Num1 = ConsoleExtension.GetInt("Enter 3 different integers: ");
     var Num2 = ConsoleExtension.GetInt();
     var Num3 = ConsoleExtension.GetInt();
+    if(Num1 == Num2)
+    {
+        Console.WriteLine(" They must be different numbers; enter them again.");
+            continue;
+     }
+    if (Num3 == Num2 || Num1 == Num3)
+    {
+        Console.WriteLine(" They must be different numbers;  enter them again.");
+        continue;
+    }
 
     if (Num2 > Num1)
     {
-        temp = Num2;
-        Num2 = Num1;
-        Num1= temp;
+        temp = Num1;
+        Num1 = Num2;
+        Num2= temp;
     }
     if(Num3 > Num2) 
     {
-        temp = Num3;
-        Num3 = Num2;
-        Num2 = temp;
+        temp = Num2;
+        Num2 = Num3;
+        Num3 = temp;
     }
 
-   if (Num3 > Num1)
+   if (Num2 > Num1)
     {
-        temp = Num3;
-        Num3 = Num1;
-        Num1 = temp;
+        temp = Num1;
+        Num1 = Num2;
+        Num2 = temp;
     }
     Console.WriteLine($"The order from greatest to least is: {Num1}, {Num2}, {Num3}");
 
