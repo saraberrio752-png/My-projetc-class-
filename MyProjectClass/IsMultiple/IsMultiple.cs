a7004fdeb84f944dc2229a967d7205b300bdc614
sara berrio <saraberrio752@gmail.com>
2026-08-16 18:56:39 -0500

    Completed exercise 04

---
+++ MyProjectClass/IsMultiple/IsMultiple.cs
@@ -0,0 +1,26 @@
+﻿using Shared;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+var answer = "Yes";
+var options = new List<string> { "Yes", "Not" };
+
+do
+{
+    var Num1 = ConsoleExtension.GetInt("Enter the first number: ");
+    var Num2 = ConsoleExtension.GetInt("Enter the second number: ");
+    if (Num2 % Num1 == 0)
+    {
+        Console.WriteLine($"The number {Num1} is a multiple of {Num2}");
+    }
+    else
+    {
+        Console.WriteLine($"The number {Num1} is not a multiple of {Num2}");
+    }
+
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Do you want to continue [ Yes ] or [ Not ]?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("Yes", StringComparison.CurrentCultureIgnoreCase));
+
+    Console.WriteLine("Game Over.");
