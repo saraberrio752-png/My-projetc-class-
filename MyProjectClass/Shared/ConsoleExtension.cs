e338847ad0a8aa197f736dd2fc64ea6ea815dfe1
sara berrio <saraberrio752@gmail.com>
2026-08-16 16:15:36 -0500

    Completed exercise 03

---
+++ MyProjectClass/Shared/ConsoleExtension.cs
@@ -0,0 +1,34 @@
+﻿namespace Shared;
+public static class ConsoleExtension
+{
+    public static int GetInt(string message)
+    {
+        Console.WriteLine(message);
+        var numberString = Console.ReadLine();
+        if (int.TryParse(numberString, out int numberInt))
+        {
+            return numberInt;
+        }
+        return 0;
+    }
+    public static int GetInt()
+    {
+        var numberString = Console.ReadLine();
+        if (int.TryParse(numberString, out int numberInt))
+        {
+            return numberInt;
+        }
+        return 0;
+    }
+    public static string? GetValidOptions(string message, List<string> options)
+    {
+        Console.Write(message);
+        var answer = Console.ReadLine();
+        if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))
+        {
+            return answer;
+        }
+
+        return null;
+    }
+}
---
+++ MyProjectClass/SortNumber/SortNumber.cs
@@ -0,0 +1,40 @@
+﻿using Shared;
+var temp =0;
+var answer = string.Empty;
+var options = new List<string> { "Yes", "Not" };
+
+do
+{
+    var Num1 = ConsoleExtension.GetInt("Enter 3 different integers: ");
+    var Num2 = ConsoleExtension.GetInt();
+    var Num3 = ConsoleExtension.GetInt();
+
+    if (Num2 > Num1)
+    {
+        temp = Num2;
+        Num2 = Num1;
+        Num1= temp;
+    }
+    if(Num3 > Num2) 
+    {
+        temp = Num3;
+        Num3 = Num2;
+        Num2 = temp;
+    }
+
+   if (Num3 > Num1)
+    {
+        temp = Num3;
+        Num3 = Num1;
+        Num1 = temp;
+    }
+    Console.WriteLine($"The order from greatest to least is: {Num1}, {Num2}, {Num3}");
+
+do
+{
+    answer = ConsoleExtension.GetValidOptions("Do you want to continue [ Yes ] or [ Not ]?: ", options);
+} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+
+} while (answer!.Equals("Yes", StringComparison.CurrentCultureIgnoreCase)) ;
+
+Console.WriteLine("Game Over.");
