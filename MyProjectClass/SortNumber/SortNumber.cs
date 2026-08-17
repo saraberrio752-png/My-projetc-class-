36e346849ebdc20d19a5f1a79eb8c61705f450d6
sara berrio <saraberrio752@gmail.com>
2026-08-16 22:23:10 -0500

    completed exercise 03

--- MyProjectClass/SortNumber/SortNumber.cs
+++ MyProjectClass/SortNumber/SortNumber.cs
@@ -1,5 +1,5 @@
 ﻿using Shared;
-var temp =0;
+var temp = 0;
 var answer = "Yes";
 var options = new List<string> { "Yes", "Not" };
 
@@ -8,11 +8,11 @@ do
     var Num1 = ConsoleExtension.GetInt("Enter 3 different integers: ");
     var Num2 = ConsoleExtension.GetInt();
     var Num3 = ConsoleExtension.GetInt();
-    if(Num1 == Num2)
+    if (Num1 == Num2)
     {
         Console.WriteLine(" They must be different numbers; enter them again.");
-            continue;
-     }
+        continue;
+    }
     if (Num3 == Num2 || Num1 == Num3)
     {
         Console.WriteLine(" They must be different numbers;  enter them again.");
@@ -23,16 +23,16 @@ do
     {
         temp = Num1;
         Num1 = Num2;
-        Num2= temp;
+        Num2 = temp;
     }
-    if(Num3 > Num2) 
+    if (Num3 > Num2)
     {
         temp = Num2;
         Num2 = Num3;
         Num3 = temp;
     }
 
-   if (Num2 > Num1)
+    if (Num2 > Num1)
     {
         temp = Num1;
         Num1 = Num2;
@@ -40,11 +40,11 @@ do
     }
     Console.WriteLine($"The order from greatest to least is: {Num1}, {Num2}, {Num3}");
 
-do
-{
-    answer = ConsoleExtension.GetValidOptions("Do you want to continue [ Yes ] or [ Not ]?: ", options);
-} while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
+    do
+    {
+        answer = ConsoleExtension.GetValidOptions("Do you want to continue [ Yes ] or [ Not ]?: ", options);
+    } while (!options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)));
 
-} while (answer!.Equals("Yes", StringComparison.CurrentCultureIgnoreCase)) ;
+} while (answer!.Equals("Yes", StringComparison.CurrentCultureIgnoreCase));
 
 Console.WriteLine("Game Over.");
